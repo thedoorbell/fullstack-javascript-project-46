42c4aa14e3156da4adbf4906cd74ac14a96e1c49
Jerry <mcgerbi4@gmail.com>
2025-06-09 17:09:55 +0300

    Update help information for gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
 
 program.parse()
