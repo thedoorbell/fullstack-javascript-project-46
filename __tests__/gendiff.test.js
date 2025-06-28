9e962dfb79556631500c97ddbab21e7734d775c2
Jerry <mcgerbi4@gmail.com>
2025-06-28 17:50:47 +0300

    Add test for plain JSON comparison & bug fix

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,18 @@
+import path from 'path'
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+import genDiff from '../src/index.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
+
+test('Compare flat JSONs', () => {
+  const file1 = getFixturePath('file1.json')
+  const file2 = getFixturePath('file2.json')
+  const expectedPath = getFixturePath('expected1.txt')
+  const expected = fs.readFileSync(expectedPath, 'utf-8')
+
+  expect(genDiff(file1, file2)).toBe(expected)
+})
