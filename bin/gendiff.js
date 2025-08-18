06318537c5a4ffb3b3d708b679e5ae4c82762a8e
Jerry <mcgerbi4@gmail.com>
2025-08-18 20:14:44 +0300

    Make a default format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,9 +8,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2))
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format))
   })
 
 program.parse()
