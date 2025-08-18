a03ccaa9ada14664ef1f7085d1fb81891f85e7c7
Jerry <mcgerbi4@gmail.com>
2025-08-18 20:13:42 +0300

    make test on jest for nested case

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,21 +7,20 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
+const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8')
 
-test('Compare flat JSONs', () => {
+test('Compare JSONs', () => {
   const file1 = getFixturePath('file1.json')
   const file2 = getFixturePath('file2.json')
-  const expectedPath = getFixturePath('expected1.txt')
-  const expected = fs.readFileSync(expectedPath, 'utf-8')
+  const expected = readFile('expected1.txt')
 
   expect(genDiff(file1, file2)).toBe(expected)
 })
 
-test('Compare flat YAMLs', () => {
+test('Compare YAMLs', () => {
   const file1 = getFixturePath('file1.yaml')
   const file2 = getFixturePath('file2.yml')
-  const expectedPath = getFixturePath('expected1.txt')
-  const expected = fs.readFileSync(expectedPath, 'utf-8')
+  const expected = readFile('expected1.txt')
 
   expect(genDiff(file1, file2)).toBe(expected)
 })
