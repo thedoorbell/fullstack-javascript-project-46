804893ec739169d20ccac0ad7b5303717157ae4e
Jerry <mcgerbi4@gmail.com>
2025-08-20 20:44:35 +0300

    Make tests & add fixture for plain format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,18 +9,16 @@ const __dirname = path.dirname(__filename)
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8')
 
-test('Compare JSONs', () => {
-  const file1 = getFixturePath('file1.json')
-  const file2 = getFixturePath('file2.json')
-  const expected = readFile('expected1.txt')
+test('Compare everything!', () => {
+  const json1 = getFixturePath('file1.json')
+  const json2 = getFixturePath('file2.json')
+  const yaml1 = getFixturePath('file1.yaml')
+  const yaml2 = getFixturePath('file2.yml')
 
-  expect(genDiff(file1, file2)).toBe(expected)
-})
+  const expectedStylish = readFile('expected1.txt')
+  const expectedPlain = readFile('expected2.txt')
 
-test('Compare YAMLs', () => {
-  const file1 = getFixturePath('file1.yaml')
-  const file2 = getFixturePath('file2.yml')
-  const expected = readFile('expected1.txt')
-
-  expect(genDiff(file1, file2)).toBe(expected)
+  expect(genDiff(json1, json2)).toBe(expectedStylish)
+  expect(genDiff(yaml1, yaml2)).toBe(expectedStylish)
+  expect(genDiff(json1, json2, 'plain')).toBe(expectedPlain)
 })
