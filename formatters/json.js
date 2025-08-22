ad43ae4d94df577225ae3a31ce5e6f94951dbed1
Jerry <mcgerbi4@gmail.com>
2025-08-22 20:52:09 +0300

    Optimize json formatter

--- formatters/json.js
+++ formatters/json.js
@@ -1,26 +1,18 @@
 import _ from 'lodash'
 
-const formatValue = (value) => {
-  if (!_.isObject(value)) {
-    return _.isBoolean(value) || _.isNull(value) ? value : `${value}`
-  }
-
-  return value
-}
-
 const buildJson = tree => Object.fromEntries(
   tree.map((node) => {
     switch (node.type) {
       case 'added':
-        return [node.key, formatValue(node.value)]
+        return [node.key, node.value]
       case 'removed':
         return [node.key, 'DELETED']
       case 'changed':
-        return [node.key, { old: formatValue(node.oldValue), new: formatValue(node.newValue) }]
+        return [node.key, { old: node.oldValue, new: node.newValue }]
       case 'nested':
         return [node.key, buildJson(node.children)]
       case 'unchanged':
-        return [node.key, formatValue(node.value)]
+        return [node.key, node.value]
       default:
         return ''
     }
